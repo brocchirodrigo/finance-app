9dd162a58b6dc49e394376bce843283040445cf1
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2022-06-05 13:22:12 -0300

    Instalação e configuração do Axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { createServer } from 'miragejs';
+
 import { App } from './App';
 import { GlobalStyle } from './styles/global';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => [
+      {
+        id: 1,
+        title: 'Desenvolvimento de site',
+        amount: 12000,
+        type: 'deposit',
+        category: 'Recebíveis',
+        date: new Date(),
+      },
+      {
+        id: 2,
+        title: 'Dog louco',
+        amount: 53.20,
+        type: 'withdraw',
+        category: 'Comida',
+        date: new Date(),
+      },
+      {
+        id: 3,
+        title: 'Aluguel',
+        amount: 1100,
+        type: 'withdraw',
+        category: 'Casa',
+        date: new Date(),
+      },
+      {
+        id: 4,
+        title: 'Participação de lucros',
+        amount: 350,
+        type: 'deposit',
+        category: 'Recebíveis',
+        date: new Date(),
+      },
+    ]);
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
